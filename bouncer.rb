1506178314ddd396916283fdddc3ce29c21dcd24
Byronj <cekcbj@gmail.com>
2015-09-08 13:32:15 -0500

    normal mode

--- bouncer.rb
+++ bouncer.rb
@@ -1,1 +1,24 @@
-def bouncer (age)
+  def bouncer
+
+    puts " what is your age"
+    age = gets.chomp.to_i
+
+  if age < 18
+    puts "you can do nothing"
+
+   else
+    if age >=  18
+      puts "you can vote"
+      puts "I can smoke"
+       if age >= 21 && age >= 25
+         puts "I can drink"
+         puts "I can rent a car"
+       elsif age >=21 && age < 25
+         puts " I can drink"
+       end
+    end
+  end
+end
+
+
+bouncer
